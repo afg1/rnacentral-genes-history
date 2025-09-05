c966805fd11753ea0cf1c1ac60a4d9049e6b25ad
Andrew Green <agreen@ebi.ac.uk>
2025-09-05 22:13:38 +0100

    Remove unneeded join

--- sql/dump_regions.sql
+++ sql/dump_regions.sql
@@ -5,7 +5,6 @@ COPY(
   sr.id as region_id
 
   FROM rnc_sequence_regions sr 
-  JOIN temp_gff_region_names rn ON rn.region_name = sr.region_name
 
   WHERE sr.urs_taxid LIKE '%_' || :'taxid'
 ) TO STDOUT WITH CSV HEADER;
