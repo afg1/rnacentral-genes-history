b7f0f54de85eef291140a4d281632dd1b0cace89
Andrew Green <agreen@ebi.ac.uk>
2025-09-07 21:23:57 +0100

    Update regions query to use ndjson

--- sql/dump_regions.sql
+++ sql/dump_regions.sql
@@ -1,10 +1,14 @@
 COPY(
   SELECT 
-  sr.region_name as region_name, 
+   json_build_object(
+        'region_name', sr.region_name,
+        'assembly_id', sr.assembly_id,
+        'region_id', sr.id,
+    )
   sr.assembly_id as assembly_id,
   sr.id as region_id
 
   FROM rnc_sequence_regions sr 
 
   WHERE sr.urs_taxid LIKE '%_' || :'taxid'
-) TO STDOUT WITH CSV HEADER;
+) TO STDOUT;
