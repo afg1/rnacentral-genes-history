2965efa479bfed9cce7d59ef43ea062bab382417
Andrew Green <agreen@ebi.ac.uk>
2025-09-07 21:27:52 +0100

    Remove training data after json object

--- sql/dump_regions.sql
+++ sql/dump_regions.sql
@@ -3,10 +3,8 @@ COPY(
    json_build_object(
         'region_name', sr.region_name,
         'assembly_id', sr.assembly_id,
-        'region_id', sr.id,
+        'region_id', sr.id
     )
-  sr.assembly_id as assembly_id,
-  sr.id as region_id
 
   FROM rnc_sequence_regions sr 
 
